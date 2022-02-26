e8a2d9dcbf7dd680d73486bcffc7567cff4f5137
Ken Chong <thienpow@aadi.my>
2022-02-26 23:30:08 +0800

    update

--- index-cae62b66679134cc.js
+++ index-cae62b66679134cc.js
@@ -284,7 +284,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-d12a9c9c1ccbaaf_bg.wasm', import.meta.url);
+        input = new URL('index-cae62b66679134cc_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
