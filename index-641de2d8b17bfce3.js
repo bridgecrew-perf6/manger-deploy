8cb92458da2856aaf41f28df3dc08d805437acdc
Ken Chong <thienpow@aadi.my>
2022-03-04 22:27:20 +0800

    update

--- index-641de2d8b17bfce3.js
+++ index-641de2d8b17bfce3.js
@@ -302,7 +302,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-9221619b3e4f54ee_bg.wasm', import.meta.url);
+        input = new URL('index-641de2d8b17bfce3_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
