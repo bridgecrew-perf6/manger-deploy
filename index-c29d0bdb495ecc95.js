b1e5bbd6e3df5952be3d2136b48bc7af848f71be
Ken Chong <thienpow@aadi.my>
2022-03-06 20:51:20 +0800

    update

--- index-c29d0bdb495ecc95.js
+++ index-c29d0bdb495ecc95.js
@@ -290,7 +290,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-aaffdb2cc3f2c755_bg.wasm', import.meta.url);
+        input = new URL('index-c29d0bdb495ecc95_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
